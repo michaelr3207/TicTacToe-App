68db669097e8b6cf91f73a98df36c577bf5180ec
michaelr3207 <michael--robinson321@hotmail.com>
2024-01-04 20:32:15 +0000

    code added to allow game and gameboard objects to be created using factories

--- app.js
+++ app.js
@@ -16,20 +16,46 @@ console.log(fancyObject);
 
 // using factory to create player objects instead of a cosntuctor
 function createPlayer(plyName){
+    let playerMarking = "";
     let score = 0;
     const increaseScore = () => score ++;
     const getScore = () => score;
-    return {plyName, increaseScore, getScore};
+    const setPlayerMarking = (marking) => playerMarking = marking;
+    const getPlayerMarking = () => playerMarking;
+    return {plyName, increaseScore, getScore, setPlayerMarking, getPlayerMarking};
 }
 
 
+function createGameCell(){
+    let currentPlayerOnCell = "";
+    let currentMarking = "";
+    const changeMarking = (marking) => currentMarking = marking;
+    const changeCurrentPlayerOnCell = (playerName) => currentPlayerOnCell = playerName;
+    const getCurrentPlayerOnCell = () => currentPlayerOnCell;
+    const getCurrentMarking = () => currentMarking;
+    return {getCurrentMarking, getCurrentPlayerOnCell, changeCurrentPlayerOnCell, changeMarking};
+}
 
+
+function createGameBoard(boardName) {
+    let cells = [];
+    const createCellsAndAddToGameboard = () => {
+        for(let index = 0; index <= 8; index ++){
+            const currentCell = createGameCell();
+            cells.push(currentCell);
+            console.log(index);
+        }
+    }
+    const getAllCells = () => cells;
+    return {getAllCells, createCellsAndAddToGameboard};
+}
 
 function createGamePlayers(player1Name, player2Name){
     const player1 = createPlayer(player1Name);
+    player1.setPlayerMarking('X');
     const player2 = createPlayer(player2Name);
-    console.log('Player 1: ' + player1.plyName);
-    console.log('Player 2: ' + player2.plyName);
+    player2.setPlayerMarking('O');
+    ticTacToe(player1, player2);
 }
 
 const gatherFormData = () => {
@@ -45,6 +71,34 @@ function addingEventListenerToButtons(){
     })
 }
 
+function createGame(gameName, player1, player2) {
+    let gamePlayers = [];
+    const addGamePlayers = () => {
+        gamePlayers.push(player1);
+        gamePlayers.push(player2);
+    }
+    const getGamePlayers = () => gamePlayers;
+    const gameBoard = createGameBoard();
+    return {gameBoard, gameName, addGamePlayers, getGamePlayers};
+}
 addingEventListenerToButtons();
+// const game = createGameBoard();
+// game.createCellsAndAddToGameboard();
+// console.log(game.getAllCells());
+// const allCells = game.getAllCells();
+// console.log(allCells[0].getCurrentMarking());
+
+function ticTacToe(player1, player2) {
+    const gameApp = createGame('Tic Tac Toe', player1, player2);
+    gameApp.gameBoard.createCellsAndAddToGameboard(); gameApp.addGamePlayers();
+    const allGameCells = gameApp.gameBoard.getAllCells();
+    const gamePlayers = gameApp.getGamePlayers();
+    console.log(allGameCells);
+    console.log(gamePlayers[1].getPlayerMarking());
+
+    console.log('CELL: ' + allGameCells[0].getCurrentMarking());
+}
+
+
 
 
